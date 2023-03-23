df6ad2a059ee667fd2f5258e110d6f1c6ce1dd2f
NaniSmarty <106944148+NaniSmarty@users.noreply.github.com>
2023-03-23 15:57:49 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python ./wesco_lotto/manage.py migrate --no-input
-python ./wesco_lotto/manage.py collectstatic --no-input
+python /home/narayanaraju/PycharmProjects/wesco/wesco_lotto/manage.py migrate --no-input
+python /home/narayanaraju/PycharmProjects/wesco/wesco_lotto/manage.py collectstatic --no-input
 
 gunicorn wesco_lotto.wsgi:application --bind 0.0.0.0:7788
